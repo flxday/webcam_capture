44b57e10bb18a977ebe6e4413d107f4e74a0ecea
felix <felix.torres86@gmail.com>
2016-10-27 16:26:32 +0200

    central black pixels

---
+++ CMakeFiles/webcam_capture.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/webcam_capture.dir/src/webcam_capture.cpp.o"
+  "webcam_capture.pdb"
+  "webcam_capture"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/webcam_capture.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
